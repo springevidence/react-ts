84076decb42a0d4bc0dfb524a0a8f39b1c71d893
springevidence <exshipbuilder1@gmail.com>
2023-06-05 17:38:39 +0300

    added controlled input, checkbox, select stories

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -23,3 +23,27 @@ export const GetValueOfUncontrolledInputByButtonsPress = () => {
         <input ref={inputRef}/> <button onClick={onClick}> save </button> - actual value: {value}
     </>
 }
+
+export const ControlledInput = () => {
+    const [value, setValue] = useState('')
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => setValue(e.currentTarget.value)
+
+    return <> <input value={value} onChange={onChange}/> - {value} </>
+}
+export const ControlledCheckbox = () => {
+    const [value, setValue] = useState(true)
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => setValue(e.currentTarget.checked)
+
+    return <input type={"checkbox"} checked={value} onChange={onChange}/>
+}
+export const ControlledSelect = () => {
+    const [value, setValue] = useState('1')
+    const onChange = (e: ChangeEvent<HTMLSelectElement>) => setValue(e.currentTarget.value)
+
+    return <select value={value} onChange={onChange}>
+        <option>none</option>
+        <option value="1">Banana</option>
+        <option value="2">Apple</option>
+        <option value="3">Lemon</option>
+    </select>
+}
